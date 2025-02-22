0c38fb53825d2f80da646396d081da5ca7831ff6
heytaru <heytaruuu@gmail.com>
2025-02-22 18:38:31 +0530

    add separations

--- basic.py
+++ basic.py
@@ -7,12 +7,15 @@ This is a multiple line comments
 # the interpeter ignore the comments.
 # most of the time we will use the comments as a guide so that the others person can known about the particular function do
 
+# -----------------------------------------------------------------------------------------------------------------------------------
 # to print some thing we can use print("")
 print("Hello...")
 
+# -----------------------------------------------------------------------------------------------------------------------------------
 # Data types
 # Python has various types of data types but the basic data types are string, boolean, integers, float.
 
+# -----------------------------------------------------------------------------------------------------------------------------------
 # variables
 # variables are the containers to store the data.
 # for example.
