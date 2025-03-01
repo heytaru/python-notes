a2c049817ded5a3b2a44c46ff11a971c7f505a9f
heytaru <heytaruuu@gmail.com>
2025-03-01 20:05:25 +0530

    fix

--- math_excerise.py
+++ math_excerise.py
@@ -5,4 +5,4 @@ import math
 # Find out the circumference of a circle by taking the radius from the user.
 rad = float(input("Enter the radius of the circle: "))
 circumference = 2 * math.pi * rad
-print(f"Circumference of the circle is: {round(circumference)}")
+print(f"Circumference of the circle is: {round(circumference * 2)}")
