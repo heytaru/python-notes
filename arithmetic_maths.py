39d3e990e07bd1b8b3cd83b5a2dc7a6ad854dbbc
heytaru <heytaruuu@gmail.com>
2025-03-01 19:11:00 +0530

    add basic math functions

--- arithmetic_maths.py
+++ arithmetic_maths.py
@@ -16,3 +16,23 @@ print(f"3 % 5 = {3 % 5}")
 
 # exponents operator
 print(f"2 ** 3 = {2 ** 3}")
+
+# Basic maths functions
+
+# round(x)
+# round off the number
+print(f"round(2.9999) = {round(2.114)}")
+
+# abs(x)
+# absolute is distance from the 0 to whole number
+print(f"abs(-4): {abs(4)}")
+
+# pow(base, exponents)
+print(f"pow(2, 3): {pow(2, 3)}")
+
+# max(x,y,z)
+# find out the maximum value
+print(f"max(10,11.5,9): {max(10,11.5,9)}")
+print(f"max(-10,-11.5,-9): {max(-10,-11.5,-9)}") # in negative the value which is closer to the 0 is bigger
+print(f"max(-10,11.5,0.9): {max(-10,11.5,0.9)}")
+
