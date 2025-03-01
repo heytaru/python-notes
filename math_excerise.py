e946ed80381e116dd9080bb753330120c79d4e7d
heytaru <heytaruuu@gmail.com>
2025-03-01 20:13:01 +0530

    add area excerise

--- math_excerise.py
+++ math_excerise.py
@@ -3,6 +3,12 @@ import math
 # Math exercises
 
 # Find out the circumference of a circle by taking the radius from the user.
+# rad = float(input("Enter the radius of the circle: "))
+# circumference = 2 * math.pi * rad
+# print(f"Circumference of the circle is: {round(circumference, 2)}")
+
+# Find out the area of circle
 rad = float(input("Enter the radius of the circle: "))
-circumference = 2 * math.pi * rad
-print(f"Circumference of the circle is: {round(circumference, 2)}")
+area = math.pi * math.pow(rad, 2)
+print(f"Area of the circle is: {round(area, 2)}cm^2")
+
