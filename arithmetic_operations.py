746687eb2489c1afc73e0a6fecd36f77e3c01d48
heytaru <heytaruuu@gmail.com>
2025-03-01 18:57:50 +0530

    add examples

--- arithmetic_operations.py
+++ arithmetic_operations.py
@@ -1,9 +1,18 @@
 # Basic arithmetic operator
 # + Addition operator
+print(f"3 + 5 = {3 + 5}")
+
 # - Subtraction operator
+print(f"3 - 5 = {3 - 5}")
+
 # * multiplication operator
+print(f"3 * 5 = {3 * 5}")
+
 # / divison operator
+print(f"3 / 5 = {3 / 5}")
+
 # % modulus operator
+print(f"3 % 5 = {3 % 5}")
 
 # exponents operator
 print(f"2 ** 3 = {2 ** 3}")
