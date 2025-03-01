2b18473d47c36be8df38ea3dea946331dc4ea700
heytaru <heytaruuu@gmail.com>
2025-03-01 19:19:42 +0530

    add basic math functions

--- arithmetic_maths.py
+++ arithmetic_maths.py
@@ -1,3 +1,5 @@
+import math
+
 # Basic arithmetic operator
 # + Addition operator
 print(f"3 + 5 = {3 + 5}")
@@ -36,3 +38,25 @@ print(f"max(10,11.5,9): {max(10,11.5,9)}")
 print(f"max(-10,-11.5,-9): {max(-10,-11.5,-9)}") # in negative the value which is closer to the 0 is bigger
 print(f"max(-10,11.5,0.9): {max(-10,11.5,0.9)}")
 
+print("\n\n\n")
+# we can use amny maths functions by importing the maths module on the top
+# import math
+
+print("Printing the value of pi")
+print(math.pi)
+
+print("Printing the value of the e")
+print(math.e)
+
+print("Printing the square root of 9")
+print(math.sqrt(9))
+
+# math.ceil()
+# it will round up the floating point number up
+print("ceil(9.50): " + str(math.ceil(9.50)))
+print("ceil(9.40): " + str(math.ceil(9.40)))
+
+# math.floor()
+# it will round down the floating point number up
+print("floor(9.50): " + str(math.floor(9.50)))
+print("floor(9.40): " + str(math.floor(9.40)))
