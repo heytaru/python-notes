97f98baca7c124e991699f2537d109b8e8045fc7
heytaru <heytaruuu@gmail.com>
2025-03-01 19:27:31 +0530

    add math exercise

---
+++ math_excerise.py
@@ -0,0 +1,8 @@
+import math
+
+# Math exercises
+
+# Find out the circumference of a circle by taking the radius from the user.
+rad = float(input("Enter the radius of the circle: "))
+circumference = 2 * math.pi * rad
+print(f"Circumference of the circle is: {round(circumference)}")
