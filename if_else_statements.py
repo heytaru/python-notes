64c55561c9b3e7239f24a4a5c0712736949f2caa
heytaru <heytaruuu@gmail.com>
2025-03-07 00:55:22 +0530

    add if_else_statements.py

---
+++ if_else_statements.py
@@ -0,0 +1,12 @@
+# IF statements
+# if condition:
+#   Perform 
+# else:
+# perform
+# example
+
+age = int(input("Enter your age: "))
+if age < 18:
+    print("You are not adult")
+else:
+    print("You are adult.")
