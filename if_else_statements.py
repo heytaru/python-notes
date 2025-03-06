9f98c5867074c4d7d364b8d66c33329d2abc5c81
heytaru <heytaruuu@gmail.com>
2025-03-07 01:01:02 +0530

    add elif code

--- if_else_statements.py
+++ if_else_statements.py
@@ -10,3 +10,19 @@ if age < 18:
     print("You are not adult")
 else:
     print("You are adult.")
+
+# elif statements
+# if condition: 
+#   perform
+# elif condition:
+#   perform
+# else:
+#   perform
+
+age = int(input("Enter age: "))
+if age >= 18:
+    print("You are an adult")
+elif age < 0:
+    print("Please enter proper age")
+else:
+    print("You are not an adult.")
